3065baca2508f88add19f227d29f97ed39011eb2
Ignatius Odi <ignodi@gmail.com>
2023-04-28 12:14:37 +0700

    crud comment

--- src/controllers/CommentController.ts
+++ src/controllers/CommentController.ts
@@ -1,13 +1,19 @@
-import { Request, Response } from "express";
-import { Comment } from "../models/index.js";
+import { NextFunction, Request, Response } from "express";
 import Joi from "joi";
+import { getUserID } from "./UserController.js";
+import { isSongExist } from "./SongController.js";
+import { Comment } from "../models/index.js";
 
 export const getComments = async (req: Request, res: Response) => {
   const comments = await Comment.findAll();
   return res.status(200).send({ comments });
 };
 
-export const createComment = async (req: Request, res: Response) => {
+export const createComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { song_id, comment }: { song_id: string; comment: string } = req.body;
   try {
     await Joi.object({
@@ -17,11 +23,30 @@ export const createComment = async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(400).send({ message: String(error) });
   }
+  try {
+    const user_id = await getUserID(req.headers.authorization as string);
+    await isSongExist(song_id, req.headers.authorization as string);
+    const createComment = await Comment.create({
+      song_id,
+      comment,
+      user_id,
+    });
+    return res.status(201).send({
+      message: "Successfully created a new comment!",
+      comment: createComment,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const updateComment = async (req: Request, res: Response) => {
-  const { comment_id } = req.params;
+export const updateComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { comment }: { comment: string } = req.body;
+  const { comment_id } = req.params;
   try {
     await Joi.object({
       comment: Joi.string().required().label("comment"),
@@ -29,15 +54,39 @@ export const updateComment = async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(400).send({ message: String(error) });
   }
+  try {
+    const commentUpdate = await Comment.findByPk(comment_id);
+    if (!commentUpdate) {
+      return res.status(404).send({ message: "Comment is not found!" });
+    }
+    commentUpdate.comment = comment;
+    await commentUpdate.save();
+    return res.status(200).send({
+      message: "Comment is successfully updated!",
+      comment: commentUpdate,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const deleteComment = async (req: Request, res: Response) => {
+export const deleteComment = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { comment_id } = req.params;
   try {
-    await Joi.object({
-      comment: Joi.string().required().label("comment"),
-    }).validateAsync(req.body);
+    const commentDelete = await Comment.findByPk(comment_id);
+    if (!commentDelete) {
+      return res.status(404).send({ message: "Comment is not found!" });
+    }
+    await commentDelete.destroy();
+    return res.status(201).send({
+      message: "Comment is successfully deleted!",
+      comment: commentDelete,
+    });
   } catch (error) {
-    return res.status(400).send({ message: String(error) });
+    next(error);
   }
 };
