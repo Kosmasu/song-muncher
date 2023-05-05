6e0e913375db06e63be929784abd5a3a21087d04
Kenny <kennylink2626@gmail.com>
2023-05-05 14:31:02 +0700

    global error handler from index to middleware

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,9 @@ import songRoute from "./routes/SongRoute.js";
 import commentRoute from "./routes/CommentRoute.js";
 import developerRoute from "./routes/DeveloperRoute.js"
 import ratingRoute from "./routes/RatingRoute.js";
-import { SpotifyAPIError } from "./exceptions/SpotifyAPIError.js";
-import { AxiosError } from "axios";
-import Joi from "joi"
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import { GlobalErrorHandler } from "./middleware/GlobalErrorHandler.js";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -35,31 +33,7 @@ app.use("/api/comment", commentRoute);
 app.use("/api/developer", developerRoute);
 
 //Error handling
-app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  console.log('typeof err:', typeof err);
-  if (err instanceof SpotifyAPIError) {
-    return res.status(err.status_code).send({
-      status: err.status_code,
-      message: err.message,
-    })
-  }
-  if (err instanceof AxiosError) {
-    return res.status(err.response?.status as number).send({
-      message: "Something went wrong while fetching datas",
-      data: err.response?.data,
-    })
-  }
-  if (err instanceof Joi.ValidationError) {
-    return res.status(400).send({
-      message: err.message,
-    });
-  }
-  return res.status(500).send({
-    status: 500,
-    message: "Something went wrong!",
-  });
-});
+app.use(GlobalErrorHandler);
 
 const port = 3000;
 app.listen(port, function () {
---
+++ src/middleware/GlobalErrorHandler.ts
@@ -0,0 +1,30 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { SpotifyAPIError } from "../exceptions/SpotifyAPIError.js";
+import { AxiosError } from "axios";
+import Joi from "joi";
+
+export const GlobalErrorHandler = (err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  console.log('typeof err:', typeof err);
+  if (err instanceof SpotifyAPIError) {
+    return res.status(err.status_code).send({
+      status: err.status_code,
+      message: err.message,
+    })
+  }
+  if (err instanceof AxiosError) {
+    return res.status(err.response?.status as number).send({
+      message: "Something went wrong while fetching datas",
+      data: err.response?.data,
+    })
+  }
+  if (err instanceof Joi.ValidationError) {
+    return res.status(400).send({
+      message: err.message,
+    });
+  }
+  return res.status(500).send({
+    status: 500,
+    message: "Something went wrong!",
+  });
+};
